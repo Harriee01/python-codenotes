14be5eaafefc9a208433dba2a528432686b40c57
Harriee01 <147805540+Harriee01@users.noreply.github.com>
2023-11-01 15:27:03 +0000

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,21 @@
+class MyClass(object):
+ var = 9
+
+# Create first instance of MyClass
+this_obj = MyClass()
+print(this_obj.var)
+
+# Another instance of MyClass
+that_obj = MyClass()
+print (that_obj.var)
+
+
+
+class Mclass(object) : 
+    var = 45
+
+    def firstM(self) :
+        print('Hello World')
+obj = Mclass()
+print(obj.var)
+obj.firstM()
